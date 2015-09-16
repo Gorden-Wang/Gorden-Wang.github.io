bb6e22a213fff7684d53c8e04e2bf1a6267b8469
Gorden <wangshiqing@koudai.com>
2015-09-16 12:49:40 +0800

    commit

---
+++ wechat/build/pages/download/index.js
@@ -0,0 +1,75 @@
+/**
+ * Created by gorden on 15/7/31.
+ */
+(function (win, $) {
+    var Person = function () {
+        this.init();
+    }
+
+    Person.prototype = {
+        init : function(){
+            var that = this;
+            that.cacheDom();
+            that.renderUI();
+            that.recacheDom();
+            that.bindEvent();
+        },
+        cacheDom : function(){
+            var that = this;
+            that.dom = {
+                wrapper : $("#page"),
+                loading : $("#loading"),
+                tpl : $("#tpl")
+            }
+        },
+        renderUI : function(){
+            var that = this;
+            that.dom.wrapper.html(juicer(that.dom.tpl.html(),{}));
+            that.dom.loading.hide();
+        },
+        recacheDom : function(){
+          var that = this;
+            that.dom.banner = $(".banner");
+        },
+        bindEvent : function(){
+            var that = this;
+
+            var thisH = window.pageYOffset,nextH= window.pageYOffset;
+            var isHide =  $("header").hasClass("hide");
+            var header = $("header");
+            $(window).on("scroll",function(event){
+                nextH = window.pageYOffset;
+                if(nextH > thisH){
+                    //页面向上走
+                    if(!isHide){
+                        header.addClass("hide");
+                    }
+                }else{
+                    //页面向下走
+
+                    if(!isHide){
+                        header.removeClass("hide");
+                    }
+                }
+                thisH = window.pageYOffset;
+            });
+
+
+            var isIphone = navigator.userAgent.match("iPhone") ? true : false;
+
+            $("#download").on("click",function(){
+                if(isIphone){
+                    $("body").append("<iframe src='https://itunes.apple.com/cn/app/yi-jian-kang/id981084179?l=en&mt=8'></iframe>")
+                }else{
+                    window.location.href = "http://a.app.qq.com/o/simple.jsp?pkgname=com.yihealth";
+                }
+            })
+
+
+        }
+    }
+
+    var person = new Person();
+
+
+})(window, $);
